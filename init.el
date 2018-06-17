4ff7368087bbf1dcc80c107126870892d528a9d2
bin <bin6160@126.com>
2018-06-17 17:54:07 +0800

    add line numbers in org markdown mode

--- init.el
+++ init.el
@@ -422,10 +422,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-line-numbers '(:relative t
                                          :disabled-for-modes dired-mode
                                          doc-view-mode
-                                         markdown-mode
-                                         org-mode
-                                         pdf-view-mode
-                                         text-mode)
+                                         pdf-view-mode)
 
    ;; Code folding method. Possible values are `evil' and `origami'.
    ;; (default 'evil)
