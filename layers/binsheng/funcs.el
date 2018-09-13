9c4f3287d3ed9ff02c40d039047af11f37a67ac0
binsheng <bin6160@126.com>
2018-09-13 10:28:23 +0800

    remove message

--- layers/binsheng/funcs.el
+++ layers/binsheng/funcs.el
@@ -2,6 +2,5 @@
 (defun binsheng/convert2py()
   (interactive)
   (let ((name (file-relative-name (buffer-file-name))))
-    (message name)
     (shell-command
      (format "pyuic5 %s -o %s.py" name (file-name-sans-extension name)))))
