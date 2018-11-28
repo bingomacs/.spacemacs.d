ada0fb2480db496e2d97ac3f2077862d03f64ce2
binsheng <bin6160@126.com>
2018-11-28 15:44:36 +0800

    add china cal

--- layers/binsheng/packages.el
+++ layers/binsheng/packages.el
@@ -39,6 +39,7 @@
     (aria2 :location (recipe :fetcher github :repo "LdBeth/aria2.el"))
     calfw
     mu4e
+    cal-china-x
     calfw-org
     easy-hugo)
   "The list of Lisp packages required by the binsheng layer.
@@ -324,5 +325,39 @@ Each entry is either:
                           mu4e-maildir-shortcuts) " OR ")
              "All inboxes" ?i)))))
 
+
+(defun binsheng/init-cal-china-x()
+  (use-package cal-china-x
+    :commands cal-china-x-setup
+    :hook (calendar-load . cal-china-x-setup)
+    :config
+    ;; `S' can show the time of sunrise and sunset on Calendar
+    (setq calendar-location-name "Chengdu"
+          calendar-latitude 30.67
+          calendar-longitude 104.06)
+
+    ;; Holidays
+    (setq calendar-mark-holidays-flag t)
+
+    (setq cal-china-x-important-holidays cal-china-x-chinese-holidays)
+    (setq cal-china-x-general-holidays
+          '((holiday-lunar 1 15 "元宵节")
+            (holiday-lunar 7 7 "七夕节")
+            (holiday-fixed 3 8 "妇女节")
+            (holiday-fixed 3 12 "植树节")
+            (holiday-fixed 5 4 "青年节")
+            (holiday-fixed 6 1 "儿童节")
+            (holiday-fixed 9 10 "教师节")))
+    (setq holiday-other-holidays
+          '((holiday-fixed 2 14 "情人节")
+            (holiday-fixed 4 1 "愚人节")
+            (holiday-fixed 12 25 "圣诞节")
+            (holiday-float 5 0 2 "母亲节")
+            (holiday-float 6 0 3 "父亲节")
+            (holiday-float 11 4 4 "感恩节")))
+    (setq calendar-holidays
+          (append cal-china-x-important-holidays
+                  cal-china-x-general-holidays
+                  holiday-other-holidays))))
 
 ;;; packages.el ends here
