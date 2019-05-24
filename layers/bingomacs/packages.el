d9ce11f1ff755290ca31cde4492bae05e3950ab6
binsheng <804154288@qq.com>
2019-05-24 14:56:47 +0800

    remove dashboard

--- layers/bingomacs/packages.el
+++ layers/bingomacs/packages.el
@@ -35,7 +35,6 @@
     leanote
     org-pomodoro
     ob-go
-    dashboard
     magit-todos
     dired-icon
     beacon
@@ -254,22 +253,7 @@ Each entry is either:
   (use-package ob-go))
 
 
-(defun bingomacs/init-dashboard()
-  (use-package dashboard
-    :ensure t
-    :config
-    (setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
-    (setq dashboard-startup-banner '"~/.spacemacs.d/img/jump.png")
-    (setq dashboard-center-content t)
-    (setq dashboard-items '((recents  . 5)
-                            (bookmarks . 5)
-                            (projects . 5)
-                            (agenda . 5)
-                            (registers . 5)))
-    (setq dashboard-set-heading-icons t)
-    (setq dashboard-set-file-icons t)
-    (setq dashboard-set-init-info t)
-    (dashboard-setup-startup-hook)))
+
 
 ;; brew install terminal-notifier
 ;; brew linkapps
