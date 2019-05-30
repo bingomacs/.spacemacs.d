87abc818d3da7e44d0f75331a54a98f0dc36d9b3
bin <bingomacs@126.com>
2019-05-30 20:54:18 +0800

    socks

--- layers/bingomacs/funcs.el
+++ layers/bingomacs/funcs.el
@@ -4,3 +4,15 @@
   (let ((name (file-relative-name (buffer-file-name))))
     (shell-command
      (format "pyuic5 %s -o %s.py" name (file-name-sans-extension name)))))
+
+
+(defun bingomacs/enable-socks()
+  (interactive)
+  (setq url-gateway-method 'socks)
+  (setq socks-server '("Default server" "127.0.0.1" 1080 5)))
+
+
+(defun bingomacs/disable-socks()
+  (interactive)
+  (setq url-gateway-method 'native)
+  (setq socks-server '("Default server" "127.0.0.1" 1080 5)))
