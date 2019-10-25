57602bfe3067a7812e183c2df81f8965a58558f5
bin <bingomacs@126.com>
2019-10-25 08:25:49 +0800

    Fix: tabnine auto complete

--- layers/devbin-tabnine/packages.el
+++ layers/devbin-tabnine/packages.el
@@ -32,7 +32,7 @@
 (defconst devbin-tabnine-packages
   '(lsp-mode
     company-box
-    company
+    ;; company
     (company-tabnine :requires company))
   "The list of Lisp packages required by the tabnine layer.
 
@@ -81,30 +81,27 @@ Each entry is either:
     )
   )
 
-(defun devbin-tabnine/post-init-company ()
-  (unless (configuration-layer/layer-used-p 'lsp)
-    (with-eval-after-load 'company
-      (push #'company-tabnine company-backends)))
-  )
+;; (defun devbin-tabnine/post-init-company ()
+;;   (unless (configuration-layer/layer-used-p 'lsp)
+;;     (with-eval-after-load 'company
+;;       (push #'company-tabnine company-backends))))
 
 
 (defun devbin-tabnine/init-company-tabnine()
   (use-package company-tabnine
     :ensure t
+    :defer t
+    :init
     :config
-    (progn
-      (setq company-tabnine-max-num-results 3)
-
-      (add-to-list 'company-transformers 'tabnine//sort-by-tabnine t)
       ;; The free version of TabNine is good enough,
       ;; and below code is recommended that TabNine not always
       ;; prompt me to purchase a paid version in a large project.
-      (defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
-        (let ((company-message-func (ad-get-arg 0)))
-          (when (and company-message-func
-                     (stringp (funcall company-message-func)))
-            (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
-              ad-do-it)))))))
+    (defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
+      (let ((company-message-func (ad-get-arg 0)))
+        (when (and company-message-func
+                   (stringp (funcall company-message-func)))
+          (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
+            ad-do-it))))))
 
 (defun devbin-tabnine/post-init-company-tabnine()
   (with-eval-after-load 'company
